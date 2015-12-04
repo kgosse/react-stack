5593cf7feb73f3f2e009bfa0ece2977c05585162
Kevin Gosse <gossekevin1@gmail.com>
2015-12-04 04:16:10 +0100

    delete example files

--- script1.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by kevin gosse on 26/11/2015.
- */
-
-module.exports = "hi there from module 1";
--- script2.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by kevin gosse on 26/11/2015.
- */
-
-var message = require('./script1');
-
-document.write(`This is formatted with ES6 ${message}`);
